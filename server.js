0a6d8e173d7cb992ebf295c586e65902bf8b150e
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-07-01 13:51:27 +0100

    New

--- server.js
+++ server.js
@@ -1,114 +1,42 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const moment = require('moment');
-const socketio = require('socket.io');
-const Turn = require('node-turn');
-const PORT = process.env.PORT || 4000;
-
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'test.html'));
+const server = require("http").Server(app);
+app.set('view engine','ejs')
+app.use(express.static('public'))
+const {v4:uuidv4} = require("uuid")
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const {ExpressPeerServer} = require("peer");
+const peerServer = ExpressPeerServer(server,{
+    debug: true
 });
 
-let rooms = {};
-let socketroom = {};
-let socketname = {};
-let micSocket = {};
-let videoSocket = {};
-let roomBoard = {};
+app.use("/peerjs",peerServer);
 
-io.on('connect', socket => {
-    socket.on("join room", (roomid, username) => {
-        socket.join(roomid);
-        socketroom[socket.id] = roomid;
-        socketname[socket.id] = username;
-        micSocket[socket.id] = 'on';
-        videoSocket[socket.id] = 'on';
+app.get('/',(req,res) => {
+  // res.send("Hello World")
+  res.redirect(`/${uuidv4()}`);
+})
 
-        if (rooms[roomid]) {
-            rooms[roomid].push(socket.id);
-            socket.to(roomid).emit('message', `${username} joined the room.`, 'Bot', moment().format("h:mm a"));
-            io.to(socket.id).emit('join room', rooms[roomid].filter(pid => pid != socket.id), socketname, micSocket, videoSocket);
-        } else {
-            rooms[roomid] = [socket.id];
-            io.to(socket.id).emit('join room', null, null, null, null);
-        }
+app.get('/:room',(req,res) => {
+    res.render("room",{roomId: req.params.room})
+})
 
-        io.to(roomid).emit('user count', rooms[roomid].length);
-    });
-
-    socket.on('action', msg => {
-        if (msg === 'mute') micSocket[socket.id] = 'off';
-        else if (msg === 'unmute') micSocket[socket.id] = 'on';
-        else if (msg === 'videoon') videoSocket[socket.id] = 'on';
-        else if (msg === 'videooff') videoSocket[socket.id] = 'off';
-
-        socket.to(socketroom[socket.id]).emit('action', msg, socket.id);
-    });
-
-    socket.on('video-offer', (offer, sid) => {
-        socket.to(sid).emit('video-offer', offer, socket.id, socketname[socket.id], micSocket[socket.id], videoSocket[socket.id]);
-    });
-
-    socket.on('video-answer', (answer, sid) => {
-        socket.to(sid).emit('video-answer', answer, socket.id);
-    });
-
-    socket.on('new icecandidate', (candidate, sid) => {
-        socket.to(sid).emit('new icecandidate', candidate, socket.id);
-    });
-
-    socket.on('message', (msg, username, roomid) => {
-        io.to(roomid).emit('message', msg, username, moment().format("h:mm a"));
-    });
-
-    socket.on('getCanvas', () => {
-        if (roomBoard[socketroom[socket.id]]) socket.emit('getCanvas', roomBoard[socketroom[socket.id]]);
-    });
-
-    socket.on('draw', (newx, newy, prevx, prevy, color, size) => {
-        socket.to(socketroom[socket.id]).emit('draw', newx, newy, prevx, prevy, color, size);
-    });
-
-    socket.on('clearBoard', () => {
-        socket.to(socketroom[socket.id]).emit('clearBoard');
-    });
-
-    socket.on('store canvas', url => {
-        roomBoard[socketroom[socket.id]] = url;
-    });
-
-    socket.on('disconnect', () => {
-        if (!socketroom[socket.id]) return;
-        socket.to(socketroom[socket.id]).emit('message', `${socketname[socket.id]} left the chat.`, `Bot`, moment().format("h:mm a"));
-        socket.to(socketroom[socket.id]).emit('remove peer', socket.id);
-        const index = rooms[socketroom[socket.id]].indexOf(socket.id);
-        rooms[socketroom[socket.id]].splice(index, 1);
-        io.to(socketroom[socket.id]).emit('user count', rooms[socketroom[socket.id]].length);
-        delete socketroom[socket.id];
-        delete socketname[socket.id];
-        delete micSocket[socket.id];
-        delete videoSocket[socket.id];
-        console.log('--------------------');
-        console.log(rooms[socketroom[socket.id]]);
-    });
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+      socket.join(roomId);
+      setTimeout(()=>{
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+      }, 1000)
+    
+    socket.on("disconnect",() => {
+        console.log("User Disconnected");
+        io.emit("user-disconnected",userId)
+    })
 });
+  });
 
-const turnServer = new Turn({
-    authMech: 'long-term',
-    credentials: {
-        emmanuel_xolani_aws: '=hB9|Dh#123'
-    },
-    listeningPort: 3478,
-    relayIps: ['13.246.44.15'],
-    externalIps: ['13.246.44.15']
-});
-
-turnServer.start();
-
-server.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+server.listen(process.env.PORT ||3030);
